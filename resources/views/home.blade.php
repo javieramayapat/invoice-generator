404d9850b6a193cd533dd83a1430a43ce226f120
Javier Amaya <Javieramayapat@gmail.com>
2021-10-19 07:40:52 -0500

    Update of home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Principal</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <h5 class="text-center">👋 Bienvenidos Superstore tu mejor lugar para las compras 📦</h5>
                 </div>
             </div>
         </div>
